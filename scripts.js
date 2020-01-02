75ac598999eba6e7ae493f4785c892a9216d9cbd
unknown <jaho.tv@gmail.com>
2020-01-02 19:44:10 +0100

    a

--- scripts.js
+++ scripts.js
@@ -1,5 +1,24 @@
-(async function(){
+const imiono = document.querySelector("#imie");
+const exp = document.querySelector("#exp")
+const id = document.querySelector('#id')
+const height = document.querySelector("#height")
+let odpowiedz = null
+
+async function pobierarka(){
 	const request = await fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
 	const response = await request.json();
-	console.log(response);
-  })()
+	odpowiedz = response
+	console.log(odpowiedz)
+	console.log(1)
+  }
+
+
+
+(async function () {
+	await pobierarka()
+	imiono.innerHTML = "Imionko: " + odpowiedz.name;
+	exp.innerHTML = "Base exp: " + odpowiedz.base_experience
+	id.innerHTML = "Id: " + odpowiedz.id
+	height.innerHTML = "Height: " + odpowiedz.height
+	//.innerHTML = "" + odpowiedz.
+})()
